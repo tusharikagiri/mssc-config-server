cf83bb25c39c130be8a11ca420aa9aab6ed0de30
tusharikagiri <126240171+tusharikagiri@users.noreply.github.com>
2025-01-24 16:27:36 +0530

    Initial Project Setup

---
+++ src/main/java/guru/sfg/brewery/mssc_config_server/MsscConfigServerApplication.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.mssc_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/sfg/brewery/mssc_config_server/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.mssc_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
